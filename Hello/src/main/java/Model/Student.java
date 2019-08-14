66faedc712ba584af84636741191570fc6e76750
hyd <ww@qq.com>
2019-08-14 11:06:53 +0800

    发布项目

---
+++ Hello/src/main/java/Model/Student.java
@@ -0,0 +1,25 @@
+package Model;
+
+public class Student {
+    
+	private Integer stuId;
+	
+	private String  stuName;
+
+	public Integer getStuId() {
+		return stuId;
+	}
+
+	public void setStuId(Integer stuId) {
+		this.stuId = stuId;
+	}
+
+	public String getStuName() {
+		return stuName;
+	}
+
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	
+}
